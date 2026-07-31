cd321bb9933138337949cddb3f3052cf2e7e2b9d
Nicolas Caliman <154644444+nicolascaliman@users.noreply.github.com>
2026-07-31 11:16:40 -0300

    atualizado footer

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,55 @@
 <link rel="stylesheet" href="css/footer.css">
-<footer>
-    <p>&copy; Copyright: PETCOMP - UFMA.</p>
+<footer class="footer">
+    <div class="footer-top">
+        <div class="footer-col">
+            <h3 class="title green">Vínculo Institucional</h3>
+            <ul>
+                <li><strong>UFMA</strong> - Universidade Federal do Maranhão</li>
+                <li><strong>CCET</strong> - Centro de Ciências Exatas e Tecnologia</li>
+                <li><strong>COCOM</strong> - Coordenação de Ciência da Computação</li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h3 class="title">Fale Conosco</h3>
+            <ul class="contact-list">
+                <li>
+                    <a href="https://share.google/a0AAijwWgabAsl6Nz">Av. dos Portugueses, 1966 - Vila Bacanga<br>
+                        São Luís - MA, CEP 65080-805</a>
+
+                </li>
+                <li>
+                    <a href="mailto:cocom@ufma.br">cocom@ufma.br</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h3 class="title">Links Rápidos</h3>
+            <ul class="link-list">
+                <li>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+                    </svg>
+                    <a href="noticias.php">Notícias e Avisos</a>
+                </li>
+                <li>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+                    </svg>
+                    <a href="documentos.php">Documentos Oficiais</a>
+                </li>
+                <li>
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+                    </svg>
+                    <a href="historico_curso.php">Sobre o Curso</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <p>&copy; Copyright: PETCOMP - UFMA.</p>
+    </div>
 </footer>
