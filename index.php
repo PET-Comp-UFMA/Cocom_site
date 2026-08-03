a0057dce2e856b010290ac498fef8fa86ccc5d69
Yan Felipe <yanfelipebp@gmail.com>
2026-08-03 11:36:16 -0300

    cabeçalho do index

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
+<?php
+
+$title = "COCOM";
+
+$cssFiles = [
+    "css/index.css"
+];
+
+require "includes/head.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>COCOM</title>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/index.css">
-</head>
 
 <body>
     <!--HEADER-->
